dcac077bbfbd2461f04b33c5e38bf12d29689d61
marco belahouane <nfk18@hotmail.co.uk>
2024-02-14 19:52:57 +0000

    adding img address to data

--- api/server.js
+++ api/server.js
@@ -9,6 +9,18 @@ app.get("/", (req, res) => {
 app.get("/recipes", (req, res) => {
   res.json(recipes);
 });
+app.get("/recipes_img", (req, res) => {
+  const images = recipes.map((recipe) => {
+    return {
+      ...recipe,
+      image:
+        recipe["image"] ||
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.refrigeratedfrozenfood.com%2Fext%2Fresources%2FNEW_RD_Website%2FDefaultImages%2Fdefault-pasta.jpg%3F1430942591&f=1&nofb=1&ipt=b84d9c4772951d49976a778d4479e44c266366cec7b1da2633d8e9c32738a8b2&ipo=images",
+    };
+  });
+  res.json(images);
+});
+
 app.get("/recipes/:id", (req, res) => {
   const id = Number(req.params.id);
   const recipe = recipes.find((item) => {
