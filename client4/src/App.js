b00a1fe0c57507a0375c18a78469f45af9a0ff3b
Marco Karim belahouane <nfk18@hotmail.co.uk>
2024-04-30 20:21:54 +0100

    adding client 4 the unmounted create react app

---
+++ client4/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./css/App.css";
+import React from "react";
+import Recipes from "./components/ImageSlider";
+import Button from "./components/button/Button";
+
+function App() {
+  return (
+    <div>
+      <Recipes />
+    </div>
+  );
+}
+
+export default App;
